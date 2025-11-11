e1b02e822c8a01ee9ccbea1f5a891453cfbc4035
sopilanazar-star <sopilanazar@gmail.com>
2025-11-11 12:02:20 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,15 +5,12 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
-    // Усі репозиторії — тільки тут
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
     }
 }
-
 rootProject.name = "maf-android-app"
 include(":app")
