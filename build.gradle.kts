12c375c9105e24651b981dd7c1f384714f507444
sopilanazar-star <sopilanazar@gmail.com>
2025-11-11 11:55:53 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-    }
-}
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
