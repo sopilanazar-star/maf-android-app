a66f7ee88a0107b41faf7404f4cea1d5c2b85e21
sopilanazar-star <sopilanazar@gmail.com>
2025-11-14 10:39:16 +0200

    Update IntroActivity.kt

--- app/src/main/java/ua/lviv/maf/IntroActivity.kt
+++ app/src/main/java/ua/lviv/maf/IntroActivity.kt
@@ -1,44 +1,62 @@
 package ua.lviv.maf
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.animation.LinearInterpolator
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class IntroActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        val ball = findViewById<ImageView>(R.id.ball)
-        val titleText = findViewById<TextView>(R.id.titleText)
+        val ball = findViewById<ImageView>(R.id.ballView)
 
-        ball.post {
-            val screenWidth = resources.displayMetrics.widthPixels.toFloat()
-            ball.translationX = -ball.width * 2f
-            val stopX = screenWidth - ball.width * 1.4f
+        // стартова позиція – м'яч трохи за краєм зліва
+        val screenWidth = resources.displayMetrics.widthPixels.toFloat()
+        ball.translationX = -screenWidth / 2f
+        ball.rotation = -20f
 
-            ball.animate()
-                .translationX(stopX)
-                .rotationBy(1080f)
-                .setDuration(1500)
-                .setInterpolator(LinearInterpolator())
-                .withEndAction {
-                    titleText.animate()
-                        .alpha(1f)
-                        .setDuration(700)
-                        .start()
-                }
-                .start()
+        // анімація руху
+        val move = ObjectAnimator.ofFloat(
+            ball,
+            "translationX",
+            ball.translationX,
+            0f
+        ).apply {
+            duration = 1500          // ~1.5 c
+            interpolator = AccelerateDecelerateInterpolator()
         }
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 3000)
+        // анімація обертання
+        val rotate = ObjectAnimator.ofFloat(
+            ball,
+            "rotation",
+            -20f,
+            720f                        // 2 повних обороти
+        ).apply {
+            duration = 1500
+            interpolator = AccelerateDecelerateInterpolator()
+        }
+
+        // після закінчення руху – відкриваємо основний екран
+        move.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                startMain()
+            }
+        })
+
+        move.start()
+        rotate.start()
+    }
+
+    private fun startMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
