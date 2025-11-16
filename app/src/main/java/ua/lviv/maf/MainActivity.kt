cfdb033383f75b8d80a56a9f4654b43e61520c3c
sopilanazar-star <sopilanazar@gmail.com>
2025-11-16 12:06:36 +0200

    Update MainActivity.kt

--- app/src/main/java/ua/lviv/maf/MainActivity.kt
+++ app/src/main/java/ua/lviv/maf/MainActivity.kt
@@ -2,6 +2,7 @@ package ua.lviv.maf
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
@@ -15,6 +16,15 @@ class MainActivity : ComponentActivity() {
         // Показуємо Splash до готовності першого кадру
         installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        // === FULLSCREEN + ховаємо статусбар і навігацію ===
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        // ==================================================
 
         val webView = WebView(this)
         setContentView(webView)
