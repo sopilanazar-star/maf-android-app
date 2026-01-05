08258b1555a3bab572e46b771ab67ab7f9d766b4
sopilanazar-star <sopilanazar@gmail.com>
2026-01-05 11:48:18 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    
+    // Додано для підтримки Firebase Analytics
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
