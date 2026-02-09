ab6097830d3af726e3f4dc971fcbc724d07b44c0
sopilanazar-star <sopilanazar@gmail.com>
2026-02-09 13:51:05 +0200

    Update MainActivity.kt

--- app/src/main/java/ua/lviv/maf/MainActivity.kt
+++ app/src/main/java/ua/lviv/maf/MainActivity.kt
@@ -1,24 +1,47 @@
 package ua.lviv.maf
 
 import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.TextView
+import android.view.View
+import android.view.WindowInsets
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // Створюємо найпростіший напис без тем і стилів
-        val testView = TextView(this).apply {
-            text = "КОНТАКТ Є! ПРОБЛЕМА В ТЕМІ (THEMES.XML)"
-            setTextColor(Color.WHITE)
-            setBackgroundColor(Color.parseColor("#007c3d"))
-            textSize = 20f
-            gravity = Gravity.CENTER
+        // 1. Повний екран
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
         }
-        
-        setContentView(testView)
+
+        // 2. Фон-градієнт
+        val gradient = GradientDrawable(
+            GradientDrawable.Orientation.TOP_BOTTOM,
+            intArrayOf(Color.parseColor("#007c3d"), Color.parseColor("#004d26"))
+        )
+        val root = RelativeLayout(this).apply { background = gradient }
+
+        // 3. НИЖНЯ НАВІГАЦІЯ (Тепер вона має знайти іконки з GitHub)
+        val nav = BottomNavigationView(this).apply {
+            id = View.generateViewId()
+            setBackgroundColor(Color.WHITE)
+            
+            // Завантажуємо твоє меню
+            inflateMenu(R.menu.bottom_nav_menu)
+
+            layoutParams = RelativeLayout.LayoutParams(-1, -2).apply {
+                addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+            }
+        }
+
+        root.addView(nav)
+        setContentView(root)
     }
 }
