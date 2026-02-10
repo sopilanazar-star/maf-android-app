9b8d03413c613224de3b6f3db63fd0d8bdaba273
sopilanazar-star <sopilanazar@gmail.com>
2026-02-10 11:58:52 +0200

    Update TournamentRow.kt

--- app/src/main/java/ua/lviv/maf/TournamentRow.kt
+++ app/src/main/java/ua/lviv/maf/TournamentRow.kt
@@ -1,8 +1,8 @@
 package ua.lviv.maf
 
 data class TournamentRow(
-    val team1: String,        // Назва першої команди або заголовок
+    val team1: String,        // Назва першої команди
     val team2: String = "",   // Назва другої команди
     val score: String = "",   // Рахунок (напр. "2 : 1")
-    val isHeader: Boolean = false // Чи це заголовок розділу
+    val isHeader: Boolean = false 
 )
