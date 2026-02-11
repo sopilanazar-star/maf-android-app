619acab8fbaafdddbc1e0e071077a76f3f433844
sopilanazar-star <sopilanazar@gmail.com>
2026-02-11 22:29:35 +0200

    Update TimelineFragment.kt

--- app/src/main/java/ua/lviv/maf/TimelineFragment.kt
+++ app/src/main/java/ua/lviv/maf/TimelineFragment.kt
@@ -1,5 +1,24 @@
 package ua.lviv.maf
+
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
-class TimelineFragment : Fragment() {
-    // Тут пізніше зробимо хронологію голів
+
+class TimelineFragment : Fragment(R.layout.fragment_timeline) {
+
+    companion object {
+        fun newInstance(matchId: String): TimelineFragment {
+            val args = Bundle()
+            args.putString("match_id", matchId)
+            val fragment = TimelineFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val matchId = arguments?.getString("match_id") ?: ""
+        // Тут буде логіка завантаження подій матчу
+    }
 }
