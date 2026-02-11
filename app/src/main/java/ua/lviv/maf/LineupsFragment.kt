fd1859ea38520116255ae48b3b18240b3249c24f
sopilanazar-star <sopilanazar@gmail.com>
2026-02-11 22:29:56 +0200

    Update LineupsFragment.kt

--- app/src/main/java/ua/lviv/maf/LineupsFragment.kt
+++ app/src/main/java/ua/lviv/maf/LineupsFragment.kt
@@ -1,5 +1,24 @@
 package ua.lviv.maf
+
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
-class LineupsFragment : Fragment() {
-    // Тут пізніше зробимо список гравців
+
+class LineupsFragment : Fragment(R.layout.fragment_lineups) {
+
+    companion object {
+        fun newInstance(matchId: String): LineupsFragment {
+            val args = Bundle()
+            args.putString("match_id", matchId)
+            val fragment = LineupsFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val matchId = arguments?.getString("match_id") ?: ""
+        // Тут буде логіка завантаження складу команд
+    }
 }
