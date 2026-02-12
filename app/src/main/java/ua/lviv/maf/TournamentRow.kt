88f49d6c971539e77c7ce2652df44e63e1b7dd58
sopilanazar-star <sopilanazar@gmail.com>
2026-02-12 16:05:59 +0200

    Update TournamentRow.kt

--- app/src/main/java/ua/lviv/maf/TournamentRow.kt
+++ app/src/main/java/ua/lviv/maf/TournamentRow.kt
@@ -1,19 +1,16 @@
 package ua.lviv.maf
 
-/**
- * Оновлена модель даних для підтримки переходів у деталі матчу.
- */
 data class TournamentRow(
-    val id: String = "",          // ID матчу з бази (для запиту деталей)
-    val team1: String = "",       // Назва першої команди
-    val logo1: String = "",       // Логотип першої команди
-    val team2: String = "",       // Назва другої команди
-    val logo2: String = "",       // Логотип другої команди
-    val score: String = "",       // Рахунок (напр. "2 : 1") або час (напр. "18:00")
-    val date: String = "",        // Дата матчу
-    val league: String = "",      // Назва турніру
-    val stage: String = "",       // Тур або етап
-    val stadium: String = "",     // Назва стадіону
-    val referee: String = "",     // Головний арбітр
-    val isHeader: Boolean = false // Прапорець для заголовка ліги
+    val id: String = "0",
+    val team1: String,
+    val team2: String,
+    val score: String,
+    val logo1: String,
+    val logo2: String,
+    val league: String,
+    val stage: String,
+    val date: String,
+    val stadium: String = "",
+    val referee: String = "",
+    val isHeader: Boolean = false
 )
