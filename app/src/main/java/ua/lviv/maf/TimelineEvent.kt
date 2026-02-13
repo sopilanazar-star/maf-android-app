4c01528a9ff5a0507ea5298bdf89529f2e69e30e
sopilanazar-star <sopilanazar@gmail.com>
2026-02-13 20:55:59 +0200

    Update TimelineEvent.kt

--- app/src/main/java/ua/lviv/maf/TimelineEvent.kt
+++ app/src/main/java/ua/lviv/maf/TimelineEvent.kt
@@ -4,5 +4,7 @@ data class TimelineEvent(
     val minute: String,
     val type: String,
     val playerName: String,
-    val teamId: String
+    val playerOutName: String? = null, // Додаємо для замін (хто вийшов)
+    val teamId: String,
+    val side: String // "left" для господарів, "right" для гостей
 )
