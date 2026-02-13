fc5a2f4a3cd6e4ae6fbb049c749836378729649e
sopilanazar-star <sopilanazar@gmail.com>
2026-02-14 00:17:15 +0200

    Update TimelineEvent.kt

--- app/src/main/java/ua/lviv/maf/TimelineEvent.kt
+++ app/src/main/java/ua/lviv/maf/TimelineEvent.kt
@@ -1,9 +1,20 @@
 package ua.lviv.maf
 
+import com.google.gson.annotations.SerializedName
+
 data class TimelineEvent(
+    @SerializedName("minute")
     val minute: String,
+
+    @SerializedName("type")
     val type: String,
-    val player_name: String,      // ТУТ МАЄ БУТИ НИЖНЄ ПІДКРЕСЛЕННЯ
-    val player_out_name: String?, // ТУТ ТЕЖ
-    val side: String
+
+    @SerializedName("player_name") // Це зв'язує JSON з кодом
+    val player_name: String,       // Тепер адаптер знайде цю змінну
+
+    @SerializedName("player_out_name")
+    val player_out_name: String?,  // Знак питання важливий!
+
+    @SerializedName("side")
+    val side: String               // Саме це відповідає за "left" або "right"
 )
