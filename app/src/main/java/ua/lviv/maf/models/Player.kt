105671ca8c6f1a9db4b2351c03cd8b5fea293963
sopilanazar-star <sopilanazar@gmail.com>
2026-02-14 22:40:29 +0200

    Create Player.kt

---
+++ app/src/main/java/ua/lviv/maf/models/Player.kt
@@ -0,0 +1,9 @@
+package ua.lviv.maf.models // Пакет має бути саме таким
+
+data class Player(
+    val id: String,
+    val name: String,
+    val number: String,
+    val position: String,
+    val photo: String
+)
