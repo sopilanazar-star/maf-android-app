76c16ca957f48c8414c2a55283d963a9f4927971
sopilanazar-star <sopilanazar@gmail.com>
2026-02-14 20:40:44 +0200

    Update StandingRow.kt

--- app/src/main/java/ua/lviv/maf/StandingRow.kt
+++ app/src/main/java/ua/lviv/maf/StandingRow.kt
@@ -1,7 +1,7 @@
 package ua.lviv.maf
 
 data class StandingRow(
-    val team_id: String,
+    val team_id: Int,
     val position: Int,
     val team_name: String,
     val logo: String,
@@ -11,5 +11,8 @@ data class StandingRow(
     val loss: Int,
     val goals_for: Int,
     val goals_against: Int,
-    val points: Int
+    val points: Int,
+    val is_group_header: Boolean = false, // Для розділення на Група А/Б
+    val group_name: String? = null,
+    val form: List<String>? = null // Для кружечків ("W", "D", "L")
 )
