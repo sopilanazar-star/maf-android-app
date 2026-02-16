67cc80bf7c793c51b1fa77769d679d5bd94a514e
sopilanazar-star <sopilanazar@gmail.com>
2026-02-16 09:14:30 +0200

    Update Player.kt

--- app/src/main/java/ua/lviv/maf/models/Player.kt
+++ app/src/main/java/ua/lviv/maf/models/Player.kt
@@ -1,9 +1,20 @@
 package ua.lviv.maf.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Player(
-    val id: String,
-    val name: String,
-    val number: String?,
-    val position: String?,
-    val photo: String?
+    @SerializedName("id")
+    val id: String? = null, // Тепер це поле не обов'язкове
+
+    @SerializedName("name")
+    val name: String? = null, // І це теж
+
+    @SerializedName("number")
+    val number: String? = null,
+
+    @SerializedName("position")
+    val position: String? = null,
+
+    @SerializedName("photo")
+    val photo: String? = null
 )
