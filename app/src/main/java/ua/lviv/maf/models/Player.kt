7967e6a8936cadc0464b99480e2a58ffd0c76018
sopilanazar-star <sopilanazar@gmail.com>
2026-02-16 09:55:27 +0200

    Update Player.kt

--- app/src/main/java/ua/lviv/maf/models/Player.kt
+++ app/src/main/java/ua/lviv/maf/models/Player.kt
@@ -3,25 +3,9 @@ package ua.lviv.maf.models
 import com.google.gson.annotations.SerializedName
 
 data class Player(
-    // Приймаємо будь-що (Any?), щоб Gson не падав від false чи чисел
-    @SerializedName("id") private val _id: Any? = null,
-    @SerializedName("name") val name: String? = null,
-    @SerializedName("number") private val _number: Any? = null,
-    @SerializedName("position") val position: String? = null,
-    @SerializedName("photo") private val _photo: Any? = null
-) {
-    // А тут магія: перетворюємо "брудні" дані в чистий текст для Адаптера
-
-    val id: String
-        get() = _id?.toString() ?: ""
-
-    val number: String
-        get() = _number?.toString() ?: ""
-
-    val photo: String
-        get() {
-            // Якщо сервер надіслав false (булеве), повертаємо пустий рядок
-            if (_photo is Boolean) return ""
-            return _photo?.toString() ?: ""
-        }
-}
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("number") val number: String,
+    @SerializedName("position") val position: String,
+    @SerializedName("photo") val photo: String
+)
