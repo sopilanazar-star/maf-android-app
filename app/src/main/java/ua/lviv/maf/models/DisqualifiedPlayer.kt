ba4c208e05fb9c575d8ca4eadf5a1bedb237d190
sopilanazar-star <sopilanazar@gmail.com>
2026-02-24 17:25:21 +0200

    Update DisqualifiedPlayer.kt

--- app/src/main/java/ua/lviv/maf/models/DisqualifiedPlayer.kt
+++ app/src/main/java/ua/lviv/maf/models/DisqualifiedPlayer.kt
@@ -3,6 +3,7 @@ package ua.lviv.maf.models
 import com.google.gson.annotations.SerializedName
 
 data class DisqualifiedPlayer(
+    @SerializedName("player_id") val playerId: String? = null, // ID для переходу в профіль
     @SerializedName("player") val name: String? = "Невідомий", 
     @SerializedName("team") val teamName: String? = "Без команди",
     @SerializedName("matches") val matches: Int? = 0,
