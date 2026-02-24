7b3b010c9e7ca32433f326e3da902812d179c64f
sopilanazar-star <sopilanazar@gmail.com>
2026-02-24 11:03:01 +0200

    Update MatchPagerAdapter.kt

--- app/src/main/java/ua/lviv/maf/MatchPagerAdapter.kt
+++ app/src/main/java/ua/lviv/maf/MatchPagerAdapter.kt
@@ -1,1 +1,21 @@
+package ua.lviv.maf
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MatchPagerAdapter(
+    activity: AppCompatActivity,
+    private val dates: List<DateModel>,
+    private val allMatches: List<TournamentRow>
+) : FragmentStateAdapter(activity) {
+
+    override fun getItemCount(): Int = dates.size
+
+    override fun createFragment(position: Int): Fragment {
+        val dateString = dates[position].date
+        // Фільтруємо всі матчі, залишаючи тільки ті, що на цю дату
+        val matchesForDate = allMatches.filter { it.date == dateString }
+        return MatchPageFragment.newInstance(matchesForDate)
+    }
+}
