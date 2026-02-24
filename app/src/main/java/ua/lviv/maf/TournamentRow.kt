ebbdc5c398fb965659ab09093bcddaa630711b23
sopilanazar-star <sopilanazar@gmail.com>
2026-02-24 11:20:50 +0200

    Update TournamentRow.kt

--- app/src/main/java/ua/lviv/maf/TournamentRow.kt
+++ app/src/main/java/ua/lviv/maf/TournamentRow.kt
@@ -1,18 +1,19 @@
 package ua.lviv.maf
 
 data class TournamentRow(
-    val id: String = "0",
-    val team1: String = "",
-    val team2: String = "",
-    val score: String = "",
-    val logo1: String = "",
-    val logo2: String = "",
-    val league: String = "",
-    val stage: String = "",
-    val date: String = "",
-    val stadium: String = "",
-    val referee: String = "",
+    val id: String = "",
     val home_team_id: String = "0",
     val away_team_id: String = "0",
-    val isHeader: Boolean = false
+    val team1: String = "",
+    val logo1: String = "",
+    val team2: String = "",
+    val logo2: String = "",
+    val score: String = "",
+    val date: String = "",
+    val league: String = "",
+    val stage: String = "",
+    val stadium: String = "",
+    val referee: String = "",
+    val isHeader: Boolean = false,
+    val status: String = "" // 🔥 ДОДАНО ЦЕЙ РЯДОК
 )
