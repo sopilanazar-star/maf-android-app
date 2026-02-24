46c43fc82c341967793ee0d74d714e73219438df
sopilanazar-star <sopilanazar@gmail.com>
2026-02-24 15:56:02 +0200

    Create RetrofitClient.kt

---
+++ app/src/main/java/ua/lviv/maf/api/RetrofitClient.kt
@@ -0,0 +1,17 @@
+package ua.lviv.maf.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://maf.lviv.ua/"
+
+    val instance: ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(ApiService::class.java)
+    }
+}
