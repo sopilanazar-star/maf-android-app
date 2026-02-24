8e4d7b0dd5e4f5e6de71fcd8fbcbc4af8287e8ba
sopilanazar-star <sopilanazar@gmail.com>
2026-02-24 17:52:09 +0200

    Update ApiService.kt

--- app/src/main/java/ua/lviv/maf/api/ApiService.kt
+++ app/src/main/java/ua/lviv/maf/api/ApiService.kt
@@ -2,12 +2,21 @@ package ua.lviv.maf.api
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 import ua.lviv.maf.models.DisqualifiedPlayer
+import ua.lviv.maf.models.Player
 
 interface ApiService {
+    // Отримання списку дискваліфікованих гравців за рік
     @GET("wp-json/maf/v1/banned-players")
     fun getDisqualifiedPlayers(
         @Query("year") year: String
     ): Call<List<DisqualifiedPlayer>>
+
+    // 🔥 НОВЕ: Отримання повної анкети гравця за його ID
+    @GET("wp-json/maf/v1/player/{id}")
+    fun getPlayerProfile(
+        @Path("id") id: String
+    ): Call<Player>
 }
