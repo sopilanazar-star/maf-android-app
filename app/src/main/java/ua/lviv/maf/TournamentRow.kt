4d7f3fe5b141a6720e52c77b964b1b8e66949941
sopilanazar-star <sopilanazar@gmail.com>
2026-02-24 14:37:04 +0200

    Update TournamentRow.kt

--- app/src/main/java/ua/lviv/maf/TournamentRow.kt
+++ app/src/main/java/ua/lviv/maf/TournamentRow.kt
@@ -1,22 +1,19 @@
 package ua.lviv.maf
 
 data class TournamentRow(
-    // 🟢 ОРИГІНАЛЬНІ ПОЛЯ (їхній порядок тепер не зламає адаптер)
     val id: String = "",
+    val home_team_id: String = "0",
+    val away_team_id: String = "0",
     val team1: String = "",
     val logo1: String = "",
     val team2: String = "",
     val logo2: String = "",
-    val score: String = "", // Тепер тут точно буде час або рахунок!
-    val date: String = "",  // А тут точно дата
+    val score: String = "", // Сюди потрапить рахунок з API
+    val date: String = "",  // Сюди потрапить "24.02.2026"
     val league: String = "",
     val stage: String = "",
-    val isHeader: Boolean = false,
-
-    // 🟡 НОВІ ПОЛЯ (в самому кінці, нічого не зсувають)
-    val home_team_id: String = "0",
-    val away_team_id: String = "0",
     val stadium: String = "",
     val referee: String = "",
+    val isHeader: Boolean = false,
     val status: String = ""
 )
