2ac44cf27e43d0b42a201bd96f37828531b5cda3
sopilanazar-star <sopilanazar@gmail.com>
2026-02-24 16:05:19 +0200

    Update Player.kt

--- app/src/main/java/ua/lviv/maf/models/Player.kt
+++ app/src/main/java/ua/lviv/maf/models/Player.kt
@@ -12,13 +12,3 @@ data class Player(
     @SerializedName("birth_date") val birthDate: String? = "",
     @SerializedName("age") val age: Int? = 0
 )
-
-// Модель для списку дискваліфікацій
-data class DisqualifiedPlayer(
-    @SerializedName("id") val id: String,
-    @SerializedName("name") val name: String,
-    @SerializedName("team_name") val teamName: String,     // Назва команди
-    @SerializedName("reason") val reason: String,           // Причина (червона, жовті тощо)
-    @SerializedName("expiry_date") val expiryDate: String,  // Дата "yyyy-MM-dd"
-    @SerializedName("year") val year: Int                   // Рік для фільтрації через спінер
-)
